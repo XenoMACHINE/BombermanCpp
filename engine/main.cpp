be6f34f95bf7b0ed9231c47403f0ea38420a6a6d
Tomilkyway <thomas.painsurget@gmail.com>
2018-07-10 00:09:33 +0200

    main engine and ai test

--- engine/main.cpp
+++ engine/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+void initSettings(){
+    std::cout << "START settings" << std::endl;
+    std::cout << "NB_BOMBS " << "5" << std::endl;
+    std::cout << "BOMB_DURATION " << "5" << std::endl;
+    std::cout << "BOMB_RADIUS " << "2" << std::endl;
+    std::cout << "STOP settings" << std::endl;
+}
+
+void initPlayers(){
+    std::cout << "START players" << std::endl;
+    std::cout << "STOP players" << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, I'm Engine!" << std::endl;
+    initSettings();
+    initPlayers();
     return 0;
 }
