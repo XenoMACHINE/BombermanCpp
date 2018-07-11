e432cd8cbff77776933ca4368377b809dcfc6dd9
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-07-11 23:59:52 +0200

    tests ok

--- ai/main.cpp
+++ ai/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <vector>
 
 int idPlayer = 0;
+int width = 0;
+int height = 0;
+std::vector<std::string> grid;
 
 void println(std::string message){
     std::cout << message << std::endl;
@@ -8,6 +12,12 @@ void println(std::string message){
 
 void println(std::string message, int number){
     std::cout << message << number << std::endl;
+}
+
+std::string input(){
+    std::string message;
+    std::getline(std::cin, message);
+    return message;
 }
 
 bool nextInputMustBe(std::string input){
@@ -36,19 +46,31 @@ int main() {
         std::string inputSetting;
         do {
             std::getline(std::cin, inputSetting);
-            // TODO : use setting
+            //TODO use settings
         } while(inputSetting != "STOP settings");
     }
 
     int turn = 1;
 
     while (true){
+        std::string message;
 
-        std::string input = "START turn " + intStr(turn);
-        if(nextInputMustBe(input)){
+        std::string expectedInput = "START turn " + intStr(turn);
+        if(nextInputMustBe(expectedInput)) {
             //TODO set grid ect
-            input = "STOP turn " + intStr(turn);
-            nextInputMustBe(input);
+            std::cin >> message;
+            width = std::stoi(message);
+            std::cin >> message;
+            height = std::stoi(message);
+
+            for (int j = 0; j < height; j++) {
+                std::getline(std::cin, message);
+                println(message);
+                grid.push_back(message);
+            }
+
+            expectedInput = "STOP turn " + intStr(turn);
+            nextInputMustBe(expectedInput);
         }
 
         println("START action ", turn);
