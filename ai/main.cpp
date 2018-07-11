6c65a172739ac5f18e620ac6ae7bbc203d91ec29
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-07-11 21:28:13 +0200

    TESTS works !!

--- ai/main.cpp
+++ ai/main.cpp
@@ -1,7 +1,59 @@
 #include <iostream>
 
+int idPlayer = 0;
+
+void println(std::string message){
+    std::cout << message << std::endl;
+}
+
+void println(std::string message, int number){
+    std::cout << message << number << std::endl;
+}
+
+bool nextInputMustBe(std::string input){
+    std::string message;
+    std::getline(std::cin, message);
+    if (message == input){
+        return true;
+    }
+    return false;
+}
+
+std::string intStr(int i){
+    return std::to_string(i);
+}
+
 int main() {
-   // std::cout << "START player" << std::endl;
-   // std::cout << "STOP player" << std::endl;
-    return 0;
+    std::string message;
+    std::getline(std::cin, message);
+
+    if(nextInputMustBe("START player"))
+        idPlayer = std::stoi(message);
+
+    nextInputMustBe("STOP player");
+
+    if(nextInputMustBe("START settings")){
+        std::string line;
+        std::getline(std::cin, line);
+        while(line != "STOP settings"){
+            std::getline(std::cin, line);
+        }
+    }
+
+    int turn = 1;
+
+    while (true){
+        std::string message;
+        std::getline(std::cin, message);
+        println(message);
+        std::string input = "START turn " + intStr(turn);
+        if(nextInputMustBe(input)){
+            //println(input);
+        }
+
+        println("START ACTION ", turn);
+        println("U");
+        println("STOP ACTION ", turn);
+        turn += 1;
+    }
 }
--- engine/main.cpp
+++ engine/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+int WIDTH = 8;
+int HEIGHT = 6;
+int DELAY = 6;
+int RADIUS = 5;
+int BOMBS = 10;
+
+int nbPlayers = 0;
+
+bool nextInputMustBe(std::string input){
+    std::string message;
+    std::getline(std::cin, message);
+    if (message == input){
+        return true;
+    }
+    return false;
+}
+
+std::string input(){
+    std::string message;
+    std::getline(std::cin, message);
+    return message;
+}
+
 void initSettings(){
     std::cout << "START settings" << std::endl;
     std::cout << "NB_BOMBS " << "5" << std::endl;
@@ -14,32 +37,71 @@ void initPlayers(){
 }
 
 void println(std::string message){
-    std::cout << message << "\n";
+    std::cout << message << std::endl;
+}
+
+void println(std::string message, int number){
+    std::cout << message << number << std::endl;
+}
+
+void sendSettings(){
+    println("START settings");
+    println("NB_BOMBS ",BOMBS);
+    println("BOMB_DURATION ",DELAY);
+    println("BOMB_RADIUS ",RADIUS);
+    println("STOP settings");
+}
+
+void sendGrid(){
+    //TEST
+    println("########");
+    println("#__1_3_#");
+    println("#___o__#");
+    println("#o_____#");
+    println("#___2__#");
+    println("#4_____#");
+    println("########");
+}
+
+std::string intStr(int i){
+    return std::to_string(i);
 }
 
 int main() {
-    std::string message;
-    std::string mode = "wait";
-    int nbPlayers = 0;
 
+    nextInputMustBe("START PLAYERS");
+    nbPlayers = std::stoi(input());
+    nextInputMustBe("STOP PLAYERS");
+
+    sendSettings();
+
+    int turn = 1;
     while (true){
-        std::getline(std::cin, message);
 
-        //println(message);
-        if(message == "START PLAYERS") {
-            mode = "player";
-        }
-        else if(message == "STOP PLAYERS") {
-            mode = "wait";
-        }
-        else if(mode == "player") {
-            nbPlayers = std::stoi(message);
-            for (int i = 0; i < nbPlayers; i++) {
-                std::cout << "init player " << i << "\n";
+        for(int i=1; i<nbPlayers+1;i++){
+            std::cout << "START turn " << turn << " " << i << std::endl;
+            if(turn == 5){
+                println("WINNER ", 3);
+            } else{
+                std::cout << WIDTH << " " << HEIGHT << std::endl;
+                sendGrid();
+            }
+            std::cout << "STOP turn " << turn << " " << i << std::endl;
+
+            std::string expectInput = "START actions " + intStr(turn) + " " + intStr(i);
+            if (nextInputMustBe(expectInput)){
+                std::string action = input();
+                //TODO exec actionsto
+                expectInput = "STOP actions " + intStr(turn) + " " + intStr(i);
+                nextInputMustBe(expectInput);
             }
         }
 
-        //println("mode " + mode);
+        if (turn == 5){
+            break;
+        }
+
+        turn++;
     }
 
     return 0;
